5a9965da9934437174f4698caaba1be6f26e35e8
Ahmad Reza Enshaee <enshaee.ahmadreza@gmail.com>
2023-03-30 16:29:49 +0400

    completed transaction items ui.

--- app/src/main/java/com/example/linenassignment/list/MainListItem.kt
+++ app/src/main/java/com/example/linenassignment/list/MainListItem.kt
@@ -1,12 +1,17 @@
 package com.example.linenassignment.list
 
 import androidx.annotation.StringRes
+import java.math.BigInteger
 
+/**
+ * Instead of directly using the currency's name and its code, I'd use [java.util.Currency]
+ * class in order to maintain a better communication with other APIs.
+ * */
 sealed interface MainListItem {
     data class Balance(
         val currencyName: String,
         val currencyCode: String,
-        val value: String
+        val value: String,
     ) : MainListItem
 
     /** Instead of passing the resource Id directly and making the ViewModel
@@ -15,5 +20,11 @@ sealed interface MainListItem {
      */
     data class Separator(@StringRes val titleResId: Int) : MainListItem
 
-    data class Transaction(val id: String) : MainListItem
+    data class Transaction(
+        val hash: String,
+        val fromAddress: String,
+        val amount: BigInteger,
+        val currencyCode: String,
+        val formattedDateTime: String,
+    ) : MainListItem
 }
